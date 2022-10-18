21607f071ae00e425cd79c777a9f5f618be39cd8
Héctor Vallvé <hectorvallveg@gmail.com>
2022-10-18 17:11:02 +0200

    boto crear incidencia

--- app/src/main/java/com/example/act2/MainActivity.java
+++ app/src/main/java/com/example/act2/MainActivity.java
@@ -2,8 +2,36 @@ package com.example.act2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.act2CrearIncidencies.CrearIncidencies;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button crearIncidencia, llistarIncidencies, resoldreIncidencies;
+
+    private Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        crearIncidencia = (Button) findViewById(R.id.botoIncidencies);
+        llistarIncidencies = findViewById(R.id.llistarIncidencia);
+        resoldreIncidencies = findViewById(R.id.resoldreIncidencia);
+
+        crearIncidencia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, CrearIncidencies.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/act2CrearIncidencies/CrearIncidencies.java
+++ app/src/main/java/com/example/act2CrearIncidencies/CrearIncidencies.java
@@ -3,7 +3,27 @@ package com.example.act2CrearIncidencies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.act2.R;
 
 public class CrearIncidencies extends AppCompatActivity {
+
+    private TextView nom, cognom, dni, email, descripcio, identificador, telContacte;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_crear_incidencies);
+        nom = findViewById(R.id.nomInformador);
+        cognom = findViewById(R.id.cognomInformador);
+        dni = findViewById(R.id.dni);
+        email = findViewById(R.id.email);
+        descripcio = findViewById(R.id.descripcioIncidencia);
+        identificador = findViewById(R.id.identificador);
+        telContacte = findViewById(R.id.telContacte);
+
+    }
 
 }
