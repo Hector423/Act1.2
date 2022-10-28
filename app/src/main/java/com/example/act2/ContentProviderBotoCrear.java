b3571b91bd348f5b864c9cb3bca6aee3791ea2d8
Héctor Vallvé <hectorvallveg@gmail.com>
2022-10-28 16:55:57 +0200

    a

--- app/src/main/java/com/example/act2/ContentProviderBotoCrear.java
+++ app/src/main/java/com/example/act2/ContentProviderBotoCrear.java
@@ -11,66 +11,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-//public class ContentProviderBotoCrear  extends AppCompatActivity {
-//    private static final String url = "jdbc:mysql://127.0.0.1:3306/M8_1.2";
-//    private static final String user = "hvallve";
-//    private static final String pass = "He123";
-//    Button btnFetch, btnClear;
-//    TextView txtData;
-//
-////    @Override
-////    protected void onCreate(Bundle savedInstanceState) {
-////        super.onCreate(savedInstanceState);
-////        setContentView(R.layout.activity_crear_incidencies);
-////        txtData = (TextView) this.findViewById(R.id.txtData);
-////        btnFetch = (Button) findViewById(R.id.btnFetch);
-////        btnClear = (Button) findViewById(R.id.btnClear);
-////        @Override
-////        public void onClick(View v) {
-////            ConnectMySql connectMySql = new ConnectMySql();
-////            connectMySql.execute("");
-////        }
-////    });
-////        btnClear.setOnClickListener(new View.OnClickListener() {
-////            @Override
-////            public void onClick(View view) {
-////                txtData.setText("");
-////            }
-////        });
-//
-//
-//    }
-//
-//
-//    private class ConnectMySql extends AsyncTask<String, Void, String> {
-//
-//        String res = "";
-//
+public class ContentProviderBotoCrear  extends AppCompatActivity {
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/M8_1.2";
+    private static final String user = "hvallve";
+    private static final String pass = "He123";
+    Button btnFetch, btnClear;
+    TextView txtData;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_crear_incidencies);
+//        txtData = (TextView) this.findViewById(R.id.txtData);
+//        btnFetch = (Button) findViewById(R.id.btnFetch);
+//        btnClear = (Button) findViewById(R.id.btnClear);
 //        @Override
-//        protected String doInBackground(String... strings) {
-//            try {
-//                Class.forName("com.mysql.jdbc.Driver");
-//                Connection con = DriverManager.getConnection(url, user, pass);
-//
-////                Statement st = con.createStatement();
-////                ResultSet rs = st.executeQuery("select nom from Programadors where pais like 'Estats Units'");
-////
-////                while (rs.next()) {
-////                    result += rs.getString(1).toString() + "\n";
-////                }
-//                String result = "Accés correcte a la BD";
-//                res = result;
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                res = e.toString();
+//        public void onClick(View v) {
+//            ConnectMySql connectMySql = new ConnectMySql();
+//            connectMySql.execute("");
+//        }
+//    });
+//        btnClear.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                txtData.setText("");
 //            }
-//            return res;
-//        }
+//        });
 //
-//        @Override
-//        protected void onPostExecute(String result) {
-//            txtData.setText(result);
-//        }
+//
 //    }
-//}
+
+
+    private class ConnectMySql extends AsyncTask<String, Void, String> {
+
+        String res = "";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                String result = "Accés correcte a la BD";
+                res = result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                res = e.toString();
+            }
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            txtData.setText(result);
+        }
+    }
+}
--- app/src/main/java/com/example/act2/MainActivity.java
+++ app/src/main/java/com/example/act2/MainActivity.java
@@ -2,8 +2,31 @@ package com.example.act2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button crearIncidencia, llistarIncidencies, resoldreIncidencies;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resoldreIncidencies = findViewById(R.id.resoldreIncidencia);
+
+        resoldreIncidencies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, ContentProviderBotoCrear.class);
+        startActivity(intent);
+    }
 
 }
