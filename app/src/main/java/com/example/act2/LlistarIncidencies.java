2fe82c2793125cb976ec1f3096ce8b60b79d6a36
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2022-11-08 15:36:44 +0100

    Cherry-picked commit from feature/llistar-incidencies to create the llistar_incidencies activity

---
+++ app/src/main/java/com/example/act2/LlistarIncidencies.java
@@ -0,0 +1,16 @@
+package com.example.act2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LlistarIncidencies extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llistar_incidencies);
+    }
+}
