e9ba93ba72728cb3ec86f37ea4b9803e68328431
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2022-11-15 17:02:42 +0100

    Something... progresses... maybe... question mark!

--- app/src/main/java/com/example/act2/LlistarIncidencies.java
+++ app/src/main/java/com/example/act2/LlistarIncidencies.java
@@ -1,16 +1,24 @@
 package com.example.act2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class LlistarIncidencies extends AppCompatActivity
 {
+    public ArrayList<Object> llistaDeRegistres;
+
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_llistar_incidencies);
+
+        recycler = findViewById(R.id.recycler);
     }
 }
