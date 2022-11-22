672f38e5ff1d3cf8cb419fd38a1ad0fa47f9c565
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2022-11-22 17:10:50 +0100

    He fet coses, suposadament

--- app/src/main/java/com/example/act2/LlistarIncidencies.java
+++ app/src/main/java/com/example/act2/LlistarIncidencies.java
@@ -3,13 +3,19 @@ package com.example.act2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
+import android.database.*;
+import android.database.sqlite;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.*;
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LlistarIncidencies extends AppCompatActivity
 {
-    public ArrayList<DatabaseListEntry> llistaDeRegistres;
+    public ArrayList<DatabaseListEntry> databaseListEntries; // <- es necessari?
 
     RecyclerView recyclerView;
 
@@ -20,5 +26,28 @@ public class LlistarIncidencies extends AppCompatActivity
         setContentView(R.layout.activity_llistar_incidencies);
 
         recyclerView = findViewById(R.id.recycler);
+
+        // aqui es on s'hauría d'omplir el databaseListEntries amb lo de la BD
+        databaseListEntries = (ArrayList<DatabaseListEntry>)getAllEntries();
+
+        // mostrar el contingut de databaseListEntries dins de recyclerView
+//        recyclerView
+    }
+
+    // things lol:
+    // -> https://stackoverflow.com/questions/39248772/how-to-add-content-in-recyclerview-dynamically#39249026
+    @SuppressWarnings("Convert2Diamond")
+    public List<DatabaseListEntry> getAllEntries()
+    {
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        databaseListEntries = new ArrayList<DatabaseListEntry>();
+        String selectQuery = "SELECT * FROM llista"; //TODO canviar el nom de la taula
+        Cursor cursor = sqLiteDatabase.rawQuery(selectQuery, null);
+
+        if(cursor.moveToFirst())
+        {
+            
+        }
+        return null;
     }
 }
